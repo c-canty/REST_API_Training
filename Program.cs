3734d1b8fd3dfe15853992f8890a2383c111d37a
Canty <chri38eb@edu.zealand.dk>
2023-10-02 22:22:55 +0200

    Do the thing

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 using MyFirstAPI.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
