0de899d97c4352673b4f4839f2a9d296cd4cd182
c-canty <christian_canty@hotmail.co.uk>
2023-10-03 12:48:07 +0200

    Done

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -25,6 +25,7 @@ namespace MyFirstAPI.Controllers
         //    return Ok(products); // Ok() is a method that returns a 200 status code with the result
         //}
 
+
         [HttpGet]
         public async Task<IActionResult> GetAllProducts()
         {             
@@ -43,24 +44,87 @@ namespace MyFirstAPI.Controllers
         }
 
 
-        //[HttpPost]
-        //public IActionResult CreateProduct()
-        //{
-        //    return Ok("Product created");
-        //}
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(Product product)
+        {
+            if (!ModelState.IsValid) // ModelState.IsValid is a property that returns true if there are no validation errors. Its not neccessary as the [ApiController] attribute does this automatically
+            {
+                return BadRequest(ModelState); // BadRequest() is a method that returns a 400 status code with the result
+            }
+            _context.Products.Add(product); // Add() is a method that adds an entity to the context
+            await _context.SaveChangesAsync(); // SaveChangesAsync() is a method that saves all changes made in this context to the database
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product); // CreatedAtAction() is a method that returns a 201 status code with the result            
+        }
 
 
-        //[HttpPut("{id}")]
-        //public IActionResult UpdateProduct(int id)
-        //{
-        //    return Ok($"Product with id {id} updated");
-        //}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product) // [FromBody] 
+        {
+            if (id != product.Id) // If the id in the URI doesn't match the id in the body, return a 400 status code with the result
+            {
+                return BadRequest($"Product with id {id} not found");
+            }
+
+            _context.Entry(product).State = EntityState.Modified; // Entry() is a method that returns an object that represents the entity being tracked by the context
+            
+            try
+            {
+                await _context.SaveChangesAsync(); 
+            }
+            catch(DbUpdateConcurrencyException) // DbUpdateConcurrencyException is thrown when an unexpected number of rows are affected during save
+            {
+                if (_context.Products.Any(p => p.Id == id) ) // If the product doesn't exist, return a 404 status code with the result
+                {
+                    return NotFound($"Product with id {id} not found");
+                }
+                else 
+                {
+                    throw; // If the product exists, throw an exception
+                }
+            } 
+            return NoContent(); // NoContent() is a method that returns a 204 status code with the result
+        }
 
 
-        //[HttpDelete("{id}")]
-        //public IActionResult DeleteProduct(int id)
-        //{
-        //    return Ok($"Product with id {id} deleted");
-        //}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id); // Find() is a method that finds an entity with the given primary key values
+            if (product == null) // If the product is not found, return a 404 status code with the result
+            {
+                return NotFound($"Product with id {id} not found");
+            }
+            _context.Products.Remove(product); // Remove() is a method that removes the entity from the context
+            await _context.SaveChangesAsync(); // SaveChangesAsync() is a method that saves all changes made in this context to the database
+            return NoContent(); // NoContent() is a method that returns a 204 status code with the result            
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProducts([FromBody] IEnumerable<int> productIds) // Takes 
+        {
+            // Check if the provided product IDs are valid and not null or empty
+            if (productIds == null || !productIds.Any())
+            {
+                // Return a 400 status code if the product IDs are null or empty
+                return BadRequest("Product IDs cannot be null or empty.");
+            }
+            // Get all the products with the provided IDs
+            var products = await _context.Products.Where(p => productIds.Contains(p.Id)).ToListAsync();
+
+            // Check if the products exist
+            if (products == null || !products.Any())
+            {   // Return a 404 status code if the products don't exist
+                return NotFound("No products found with the provided IDs.");
+            }
+            // Remove the products from the context
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
-﻿
+﻿// # nullable disable   to disable nullability for the entire file
 
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace MyFirstAPI.Models
@@ -7,15 +8,19 @@ namespace MyFirstAPI.Models
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
+        [Required]
         public string Sku { get; set; } = string.Empty;
 
+        [Required]
         public int CategoryId { get; set; }
         [JsonIgnore] // This is to prevent a circular reference
-        public virtual Category Category { get; set; }
+        public virtual Category? Category { get; set; }
 
     }
 }
